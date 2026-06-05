37d440c67d01982fd0f4b46c25cfb8e3cbc79e7b
kaidngat4-droid <kaidngat4@gmail.com>
2026-06-06 02:28:56 +0300

    Add files via upload
    رفع ملفات المشروع

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = 'medsearch-v3';
+const ASSETS = [
+  '/',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => 
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request).catch(() => {
+      if (e.request.mode === 'navigate') {
+        return caches.match('/');
+      }
+    }))
+  );
+});
